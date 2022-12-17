d25558dc2e7763f288adb409505a595a3e7afa59
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2022-12-17 23:38:58 +0900

    코드업 기초 100제

--- CodeUpBasic/6098.py
+++ CodeUpBasic/6098.py
@@ -1,20 +1,31 @@
-def showStatus():
+def showStatus(h: int):
     for i in range(h):
         print(*MAP[i])
 
 
-h, w = map(int, input().split())
-MAP = [[0]*w for _ in range(h)]
-n = int(input())
+MAP_SIZE = 10
+MAP = [[0]*MAP_SIZE for _ in range(MAP_SIZE)]
 
-for _ in range(n):
-    #참고로 x가 세로고 y과 x라고 제시되어, y,x 입력 순서를 바꿈
-    l, d, y, x = map(int, input().split())
-    if (d == 0):  # 가로
-        for i in range(l):
-            MAP[y-1][x-1+i] = 1
-    elif (d == 1):  # 세로
-        for j in range(l):
-            MAP[y-1+j][x-1] = 1
-showStatus()
-
+for i in range(MAP_SIZE):
+    MAP[i] = list(map(int, input().split()))
+currentX = 1
+currentY = 1
+MAP[currentY][currentX] = 9
+while (True):
+    if (MAP[currentY][currentX+1] == 0):
+        MAP[currentY][currentX] = 9
+        currentX += 1
+    elif (MAP[currentY+1][currentX] == 0):
+            MAP[currentY][currentX] = 9
+            currentY += 1
+    elif (MAP[currentY+1][currentX] == 1 and MAP[currentY][currentX+1] == 1 ):
+        MAP[currentY][currentX] = 9
+        break
+    else:
+        if (MAP[currentY][currentX+1] == 2):
+            MAP[currentY][currentX] = MAP[currentY][currentX+1] = 9
+            break
+        elif (MAP[currentY+1][currentX] == 2):
+            MAP[currentY][currentX] = MAP[currentY+1][currentX] = 9
+            break
+showStatus(MAP_SIZE)
