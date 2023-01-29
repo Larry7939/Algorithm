adebb3cbf68e227ae94162bd2b09ca70ec0d656c
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-01-29 20:04:22 +0900

    그리디 - 세 수, 두 M

--- 그리디/세 수, 두 M.py
+++ 그리디/세 수, 두 M.py
@@ -2,6 +2,8 @@ import sys
 
 
 def f(a, b, c):
+    # 평균x3 = a + b + c
+    # 중앙값x3 = 3*b
     return abs(a + b + c - 3 * b)
 
 
@@ -12,6 +14,7 @@ A.sort()
 
 answer = -1
 for i in range(1, N-1):
-    answer = max(answer, f(A[i-1], A[i], A[N-1]))
-    answer = max(answer, f(A[0], A[i], A[i+1]))
+    # 중앙값이 A[i]인 경우
+    answer = max(answer, f(A[i-1], A[i], A[N-1]))  # 평균이 최대인 경우
+    answer = max(answer, f(A[0], A[i], A[i+1]))  # 평균이 최소인 경우
 print(answer)
