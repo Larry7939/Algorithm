d2dc3fd7d7a01beff2cebee394d70de1ebdcb7e9
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-02-05 22:00:12 +0900

    DFS - 바이러스

---
+++ DFS&BFS/바이러스.py
@@ -0,0 +1,29 @@
+# BOJ 2606 바이러스
+
+# 컴퓨터의 수 입력
+N = int(input())
+# 쌍의 수 입력
+M = int(input())
+# 한 번에 한 쌍씩 입력
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    x, y = list(map(int, input().split()))
+    graph[x].append(y)
+    graph[y].append(x)
+visited = [False]*(N+1)
+num = 0
+result=[0]
+
+def dfs(v, num):
+    # visited = true
+    # num += 1
+    # 반복문 N
+    # 만약 visited[i]가 true가 아니라면
+    # dfs(v,num)
+    visited[v] = True  # 방문 처리
+    result[0] += 1
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(i, num)
+dfs(1, num)
+print(result[0]-1)
