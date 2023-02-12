10025ce92e1ae9e77f5e321f763e244f60169af5
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-02-12 23:59:46 +0900

    DFS - 작업

---
+++ DFS/작업.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+#BOJ 21937
+
+sys.setrecursionlimit(10**7)
+
+def dfs(v):
+    global answer
+    visited[v]=1
+    for i in graph[v]:
+        if visited[i] == 0:
+            answer +=1
+            dfs(i)
+
+#입력부
+#작업 개수(N), 순서 정보 개수(M)
+N,M = map(int,input().split())
+visited = [0]*(N+1)
+answer = 0
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    x,y = map(int,input().split())
+    graph[y].append(x)
+
+#목적지 입력
+v = int(input())
+
+dfs(v)
+print(answer)
