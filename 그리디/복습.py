1ddf98265d1ab5929cbe72289e7e8b530f4d593f
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-06-01 21:24:10 +0900

    6/1 무지의 먹방 라이브

--- 그리디/복습.py
+++ 그리디/복습.py
@@ -1,19 +1,31 @@
-import sys
-# 볼링공 고르기
-n,m = map(int,sys.stdin.readline().rstrip().split())
-balls = list(map(int,sys.stdin.readline().rstrip().split()))
-balls.sort()
-answer = 0
-
-# 1 2 2 3 3
-# 1, 2, 2
-# 매 공마다, 더 높은 무게들의 개수만큼 더해준다.
-weight_list = [0]*(m+1)
-for i in range(1,m+1):
-    weight_list[i] = balls.count(i)
-
-for i in range(1,m+1):
-    n -= weight_list[i]
-    answer += weight_list[i] * n
+import heapq
+# 무지의 먹방 라이브
+def solution(food_times, k):
+    if sum(food_times) <= k:
+        return -1
     
-print(answer)
+    q = []
+
+    for i in range(len(food_times)):
+        heapq.heappush(q,(food_times[i],i+1))
+    
+    now_food = 0
+    before_food = 0
+    all_food_count = len(food_times)
+    cur_sum = 0
+    while cur_sum + (q[0][0] - before_food) * all_food_count <= k:
+        now_food = heapq.heappop(q)
+        cur_sum += (now_food[0] - before_food) * all_food_count
+        all_food_count -= 1
+        before_food = now_food[0]
+    
+    result = sorted(q,key=lambda x: x[1])
+    return result[(k-cur_sum) % all_food_count][1]
+
+
+
+print(solution([3,1,2],5))
+# 1 2 3 , cur_sum = 0
+# 0 1 2 , cur_sum = 3
+# 0 0 1 , cur_sum = 5
+# k = 5 , 
