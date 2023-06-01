6c995b08aa5045c26316d11c4d7f669650482f58
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-06-01 15:01:36 +0900

    6/1 1이 될 때까지

--- 그리디/1이 될 때까지_2.py
+++ 그리디/1이 될 때까지_2.py
@@ -22,3 +22,22 @@ result += (n-1) #n이 0이 아닌 1이 되는 것이 목표이므로 n-1
 print(result)
 
 
+# import sys
+# # 1이 될 때까지
+# # 1. N에서 1을 뺀다
+# # 2. N을 K로 나눈다.
+
+# n,k = map(int,sys.stdin.readline().split())
+# result = 0
+
+# # while문을 돌면서 n이 k로 나누어 떨어지면 2번, 그렇지 않으면 1번을 수행한다.
+# while n != 1:
+#     if n % k == 0:
+#         n = n//k
+#         result += 1
+#     else:
+#         result += n%k
+#         n -= n%k
+        
+# print(result)
+# # 1번 과정보다는 2번 과정을 최대한 많이 수행하여, 빠르게 N을 감소시키는 것이 핵심
--- 그리디/복습.py
+++ 그리디/복습.py
@@ -1,9 +1,19 @@
 import sys
-# 숫자 카드 게임
+# 1이 될 때까지
+# 1. N에서 1을 뺀다
+# 2. N을 K로 나눈다.
 
-n,m = map(int,sys.stdin.readline().split())
-result = -1
-for _ in range(n):
-    row = list(map(int,sys.stdin.readline().split()))
-    result = max(result,min(row))
+n,k = map(int,sys.stdin.readline().split())
+result = 0
+
+# while문을 돌면서 n이 k로 나누어 떨어지면 2번, 그렇지 않으면 1번을 수행한다.
+while n != 1:
+    if n % k == 0:
+        n = n//k
+        result += 1
+    else:
+        result += n%k
+        n -= n%k
+        
 print(result)
+# 1번 과정보다는 2번 과정을 최대한 많이 수행하여, 빠르게 N을 감소시키는 것이 핵심
