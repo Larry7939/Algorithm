1b12f0bebd6fce59315ea6222c9f4c02c7f0d73f
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-06-01 23:47:55 +0900

    6/1 차집합/리스트 컴프리헨션

--- 그리디/복습.py
+++ 그리디/복습.py
@@ -1,31 +1,14 @@
-import heapq
-# 무지의 먹방 라이브
-def solution(food_times, k):
-    if sum(food_times) <= k:
-        return -1
-    
-    q = []
-
-    for i in range(len(food_times)):
-        heapq.heappush(q,(food_times[i],i+1))
-    
-    now_food = 0
-    before_food = 0
-    all_food_count = len(food_times)
-    cur_sum = 0
-    while cur_sum + (q[0][0] - before_food) * all_food_count <= k:
-        now_food = heapq.heappop(q)
-        cur_sum += (now_food[0] - before_food) * all_food_count
-        all_food_count -= 1
-        before_food = now_food[0]
-    
-    result = sorted(q,key=lambda x: x[1])
-    return result[(k-cur_sum) % all_food_count][1]
+a = set([1,2,3,4,5])
+b = set([2,4])
+a = a-b
+print(type(a))
+print(a)
 
 
+lost = [0,1,2,3]
+reserve = [1,2,3,4]
 
-print(solution([3,1,2],5))
-# 1 2 3 , cur_sum = 0
-# 0 1 2 , cur_sum = 3
-# 0 0 1 , cur_sum = 5
-# k = 5 , 
+_lost = [l for l in lost if l not in reserve]
+_reserve = [r for r in reserve if r not in lost]
+print(_lost)
+print(_reserve)
