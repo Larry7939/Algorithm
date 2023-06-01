73d7cf7f3abe414faa5a06f781207ee1a1da1b6a
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-06-01 18:05:40 +0900

    6/1 만들 수 없는 금액

--- 그리디/복습.py
+++ 그리디/복습.py
@@ -1,13 +1,14 @@
 import sys
-# 곱하기 혹은 더하기
-s = sys.stdin.readline().rstrip()
-s = list(map(int,''.join(s)))
+#만들 수 없는 금액
 
-answer= s[0]
-
-for i in range(1,len(s)):
-    if s[i] <= 1 or answer <= 1:
-        answer += s[i]
-    else:
-        answer *= s[i]
-print(answer)
+# 1 1 2 3 9
+# 다음 동전이 현재까지의 합보다 크면, 만들 수 없는 금액이다.
+n = int(sys.stdin.readline())
+coins = list(map(int,sys.stdin.readline().rstrip().split()))
+coins.sort()
+target = 1 
+for coin in coins:
+    if target < coin:
+        break
+    target += coin
+print(target)
