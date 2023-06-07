51479d9d0809ac729b0891fcf695c6e432b075e2
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-06-07 20:14:48 +0900

    6/7 스네이크버드

---
+++ 그리디/스네이크버드.py
@@ -0,0 +1,15 @@
+#BOJ 16435 스네이크버드
+import sys
+#과일 높이 정렬
+#스네이크버드의 길이보다 작거나 같은 경우, 길이 + 1
+
+n,l = map(int,sys.stdin.readline().split())
+h = list(map(int,sys.stdin.readline().split()))
+h.sort()
+for i in h:
+    if l >= i:
+        l+=1
+    else:
+        break
+
+print(l)
