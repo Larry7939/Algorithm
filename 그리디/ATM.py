ea7e86829b2c1b622d5ea7948dd855b787f81890
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-06-07 20:14:19 +0900

    6/7 ATM

---
+++ 그리디/ATM.py
@@ -0,0 +1,18 @@
+#BOJ 11399 ATM
+import sys
+n = int(sys.stdin.readline())
+p = list(map(int,sys.stdin.readline().split()))
+p.sort()
+#돈을 인출하는 데에 걸리는 시간을 정렬 시킨 후, 가장 작은 시간부터 누적합을 구하는 것이 핵심
+sum = 0
+answer = 0
+for i in p:
+    sum += i
+    answer += sum
+print(answer)
+
+# 3 1 4 3 2
+# 3 4 8 11 13 -> 39
+
+# 1 2 3 3 4
+# 1 3 6 9 13 -> 32
