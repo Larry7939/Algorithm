b31468a78afaca60468bf6ed8c36da1348d04759
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-06-13 15:21:32 +0900

    6/13 구현 복습 - 시각

--- 구현/복습.py
+++ 구현/복습.py
@@ -1,17 +1,13 @@
 import sys
 n = int(sys.stdin.readline())
-plans = list(sys.stdin.readline().split())
-
-x, y = 1, 1
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-move_type = ['U', 'D', 'L', 'R']
-for plan in plans:
-    for i in range(len(move_type)):
-        if plan == move_type[i]:
-            nx, ny = x+dx[i], y+dy[i]
-    if nx<1 or nx > n or ny < 1 or ny > n:
-        continue
-    else:
-        x,y = nx,ny
-print(y,x)
+h = n+1
+m = 60
+s = 60
+answer = 0
+for i in range(h):
+    for j in range(m):
+        for k in range(s):
+            if '3' in str(i)+str(j)+str(k):
+                print(f"{i}:{j}:{k}")
+                answer += 1
+print(answer)
