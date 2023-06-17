65d3a317571f8acb185f65521169cbc8c21791be
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-06-17 19:30:21 +0900

    구현 - 시각

--- 구현/시각.py
+++ 구현/시각.py
@@ -1,11 +1,24 @@
+#BOJ 18312 시각
+import sys
 #정수 N입력(0<=N<=23)
-# 3이 하나라도 포함되어있는 시각의 개수 출력
-
+# a가 하나라도 포함되어있는 시각의 개수 출력
+# 반례 - a가 0인 경우에 대한 고려 필수
 count=0
-n = int(input())
+n,a = map(int,sys.stdin.readline().split())
 for i in range(0,n+1):
     for j in range(60):
         for k in range(60):
-            if '3' in str(i)+str(j)+str(k):
+            h = str(i)
+            m = str(j)
+            s = str(k)
+            if i < 10:    
+                h = '0' + h
+            if j < 10:
+                m = '0' + m
+            if k < 10:
+                s = '0' + s
+            
+            result = h+m+s
+            if str(a) in result:
                 count+=1
 print(count)
