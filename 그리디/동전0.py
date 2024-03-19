dbd70603cb069b6627696e520e2dd1dc46564aa9
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2024-03-19 16:12:28 +0900

    그리디 - 동전0

--- 그리디/동전0.py
+++ 그리디/동전0.py
@@ -1,16 +1,17 @@
+# 풀이 횟수 2
 #BOJ 11047 동전 0
-import sys
-from collections import deque
-n, k = map(int,sys.stdin.readline().split())
-coins = deque()
-answer = 0
-#동전을 내림차순으로 정렬한 후, 몫 및 나머지 연산
-for _ in range(n):
-    coins.appendleft(int(sys.stdin.readline()))
 
+# N개 종류 동전으로 K 값을 만들기 위해 필요한 동전의 최소 개수
+# 거스름돈 문제와 동일하게, 가장 큰 동전으로 먼저 연산하고, 나머지 연산을 해서 반영하면 된다.
+
+N,K = map(int,input().split())
+coins = []
+for _ in range(N):
+    coins.append(int(input()))
+coins.sort(reverse=True)
+answer = 0
 for coin in coins:
-    if k >= coin:
-        answer += k//coin
-        k = k % coin
-        
+    if coin <= K:
+        answer += K // coin
+        K %= coin
 print(answer)
