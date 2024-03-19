596a2c6477208e6539795cb42e38d0c74a927979
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2024-03-19 13:30:27 +0900

    그리디 - 거스름돈

--- 그리디/거스름돈boj.py
+++ 그리디/거스름돈boj.py
@@ -1,3 +1,5 @@
+# 풀이 횟수 2
+
 #BOJ 5585 거스름돈
 import sys
 paid_money = 1000
@@ -6,8 +8,7 @@ coins = [500,100,50,10,5,1]
 change_money = paid_money - money
 answer = 0
 #paid_money에서 지불해야하는 돈을 뺀 나머지(거스름돈)를 동전으로 몫 및 나머지 연산
-for i in coins:
-    if change_money >= i:
-        answer += change_money//i
-        change_money = change_money % i
+for coin in coins:
+    answer += change_money // coin
+    change_money %= coin
 print(answer)
