8271f3c5f4d328614130b2151244ffaf5e9cc0e7
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2024-05-04 13:38:52 +0900

    그리디 - 보물

---
+++ 그리디/보물_3.py
@@ -0,0 +1,9 @@
+N = int(input())
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+a.sort()
+b.sort(reverse=True)
+result = 0
+for i in range(N):
+    result += a[i]*b[i]
+print(result)
