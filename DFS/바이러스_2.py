cc5b0384e9b06302b23956a21db9fe0b0f085093
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2025-05-13 17:09:11 +0900

    DFS - 바이러스

--- DFS/바이러스_2.py
+++ DFS/바이러스_2.py
@@ -2,7 +2,7 @@
 # 컴퓨터 수 N
 # 컴퓨터 쌍의 수 M
 # 각 컴퓨터 번호를 인덱스로 하는 리스트에 연결된 컴퓨터를 append한다.
-# dfs 함수에서 만약 인자를 타고 오는 infected 리스트에 True라고 되어있는 경우에 한해서만 dfs를 재귀호출한다.
+# dfs 함수에서 만약 인자를 타고 오는 infected 리스트에 False라고 되어있는 경우에 한해서만 dfs를 재귀호출한다.
 # 만약 인자로 전달된 것의 infected가 True인 경우 return
 # False인 경우 True로 변경
 
