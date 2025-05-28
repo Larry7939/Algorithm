002b835b79994545ebcb51d462c4e26c40437a14
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2025-05-28 17:46:41 +0900

    백트래킹 - N과 M(1)

--- 백트래킹/N과 M (1)_2.py
+++ 백트래킹/N과 M (1)_2.py
@@ -1,4 +1,5 @@
 #BOJ 15649 N과 M (1)
+#풀이 횟수: 2
 #백트래킹 풀이
 import sys
 N,M = map(int,sys.stdin.readline().split())
@@ -7,6 +8,7 @@ s = []
 def backTracking():
     if len(s) == M:
         print(*s)
+        return
     for i in range(1,N+1):
         if i not in s:
             s.append(i)
